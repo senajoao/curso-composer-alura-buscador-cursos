b7cd87ed5dc9d0585195f2d80082037e9239e0ac
okakmeqa <okakmeqa@gmail.com>
2020-04-03 08:11:05 -0300

    first commit

---
+++ Teste.php
@@ -0,0 +1,16 @@
+<?php
+
+class Teste
+{
+    public static function metodo() {
+        echo "Teste";
+    }
+}
+
+class Teste2
+{
+    public static function metodo() {
+        echo "Teste2";
+    }
+}
+
---
+++ buscar-curso.php
@@ -0,0 +1,19 @@
+<?php
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+use Alura\BuscadorDeCursos\Buscador;
+
+$client = new Client([
+    'base_uri' => 'https://www.alura.com.br/',
+    'verify' => false
+]);
+$crawler = new Crawler();
+
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach ($cursos as $curso) {
+    echo exibeMensagem($curso);
+}
